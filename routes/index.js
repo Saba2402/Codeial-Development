31f67bf9c24d2e15150edf67eda18b7be6b781d8
Saba Parveen <sabaparveen0112@gmail.com>
2022-07-23 16:33:11 +0530

    set other router and controller for users

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,9 @@ const homeController = require('../controllers/home_controller');
 console.log('router loaded');
 
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
+
+//for any further routes,access from here
+//router.use('/routerName',require('./routerfile'));
 
 module.exports=router;
