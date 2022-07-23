4811ecd922f243eb725220fcf0ca59e543730ad8
Saba Parveen <sabaparveen0112@gmail.com>
2022-07-24 01:31:19 +0530

    sign in and sign up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,19 @@
 const User = require('../models/user');
 
 module.exports.profile = function(req,res){
-    return res.render('user_profile',{
-        title : 'User Profile'
-    })
+    if(req.cookies.user_id){
+       User.findById(req.cookies.user_id,function(err,user){
+        if(user){
+            return res.render('user_profile',{
+                title:"User Profile",
+                user:user
+            })
+        }
+        return res.redirect('/users/sign-in');
+       });
+    }else{
+       return res.redirect('/users/sign-in');
+    }
 }
 
 //add some actions
@@ -43,6 +53,28 @@ module.exports.create = function(req,res){
 }
 
 //sign in and create a session for the user
-module.exports.createSession = function(req,res){
-    //
+module.exports.createSession = function(req, res){
+
+    // steps to authenticate
+    // find the user
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('error in finding user in signing in'); return}
+        // handle user found
+        if (user){
+
+            // handle password which doesn't match
+            if (user.password != req.body.password){
+                return res.redirect('back');
+            }
+
+            // handle session creation
+            res.cookie('user_id', user.id);
+            return res.redirect('/users/profile');
+
+        }else{
+            // handle user not found
+
+            return res.redirect('back');
+        }
+    });
 }
--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
 const port  = 8000;
-//const expressLayouts = require('express-ejs-layouts');
+const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 
 app.use(express.urlencoded());
 
 app.use(cookieParser());
 
-app.use(express.static('./assets'));
+app.use("/assets",express.static(__dirname + '/assets'));
 
-//app.use(expressLayouts);
+app.use(expressLayouts);
 // extract style and scripts from sub pages into the layout
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
