e7ea3b01f8aa3887faf77fcf1637b9e9350ccb15
Saba Parveen <sabaparveen0112@gmail.com>
2022-07-23 21:59:33 +0530

    user sign up

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
 const port  = 8000;
-const expressLayouts = require('');
+//const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 
 app.use(express.urlencoded());
@@ -12,7 +12,7 @@ app.use(cookieParser());
 
 app.use(express.static('./assets'));
 
-app.use(expressLayouts);
+//app.use(expressLayouts);
 // extract style and scripts from sub pages into the layout
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
