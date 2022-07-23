f559de35407ec1cf1f13775bba31f1bb2b60e4e3
Saba Parveen <sabaparveen0112@gmail.com>
2022-07-23 17:57:53 +0530

    adding signin and signup page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,20 @@
 module.exports.profile = function(req,res){
-    res.end('<h1>User Profile</h1>');
+    return res.render('user_profile',{
+        title : 'User Profile'
+    })
+}
+
+//add some actions
+//render the sign up page
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title : "Chatting Engine | Sign Up"
+    })
+}
+
+//render the sign in page
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title : "Chatting Engine | Sign In"
+    })
 }
