133a9931edbb42e9a216504d791e45aabbdd377d
Saba Parveen <sabaparveen0112@gmail.com>
2022-07-27 20:17:01 +0530

    sending posts and comments and showing them in the home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,33 @@
+const Post = require('../models/post');
+const { post } = require('../routes');
+
 module.exports.home =function(req,res){
-    console.log(req.cookies);
-    res.cookie('user_id', 25);
-    return res.render('home',{
-        title : "Home"
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
+
+    // Post.find({},function(err,posts){
+    //     return res.render('home',{
+    //         title : "Codeial | Home",
+    //         posts : posts
+    //     });
+    // });
+
+    //populate the user of each post
+    Post.find({})
+    .populate('user')
+    .populate({
+        path : 'comments',
+        populate :{
+            path : 'user'
+        }
+    })
+    .exec(function(err,posts){
+        return res.render('home',{
+            title : "Codeial | Home",
+            posts : posts
+        });
     });
+    
 }  
 
 //module.exports.actionName = function(req,res){}
